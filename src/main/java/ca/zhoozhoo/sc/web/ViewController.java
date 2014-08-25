5dd1e2258f1fc77f27fa3429885912c27e042557
zhoozhoo <zhoozhoo@yahoo.com>
2014-08-24 22:44:12 -0400

    Added JSON and JQuery support.

--- src/main/java/ca/zhoozhoo/sc/web/ViewController.java
+++ src/main/java/ca/zhoozhoo/sc/web/ViewController.java
@@ -3,9 +3,12 @@ package ca.zhoozhoo.sc.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ca.zhoozhoo.sc.model.Product;
 import ca.zhoozhoo.sc.repository.ProductRepository;
@@ -19,11 +22,13 @@ public class ViewController {
 	@RequestMapping("/products")
 	public String products(Model model) {
 
-		List<Product> products = productRepository.findAll();
-		for (Product product : products) {
-			System.out.println(product);
-		}
-		
 		return "products";
+	}
+
+	@RequestMapping(value = "/getAllProducts", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Product> getAllProducts() {
+		List<Product> products = productRepository.findAll();
+
+		return products;
 	}
 }
--- src/main/webapp/WEB-INF/jsp/products.jsp
+++ src/main/webapp/WEB-INF/jsp/products.jsp
@@ -1,7 +1,12 @@
-<!DOCTYPE html>
-<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
-<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
-<html lang="en">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<html>
+<head>
+<title>Home</title>
+<script type="text/javascript" src="<c:url value='/resources/jquery-2.1.1.js' /> "></script>
+</head>
 <body>
+
 </body>
 </html>
